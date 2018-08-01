f30ec88e8f59d40432ddbed394a93abcb9662c29
ATimaru <malmsteen99@gmail.com>
2018-08-01 22:21:44 +0300

    Added an expect method to verify if the new element displays after being added.

--- test.js
+++ test.js
@@ -6,25 +6,35 @@ describe('Protractor Test', function() {
     var decideLater = element(by.css('body > div.geDialog > div > div:nth-child(3)'));
     decideLater.click();
     browser.sleep(1000);
+
     var editDataButton = element(by.css('body > div.geSidebarContainer.geFormatContainer > div > div:nth-child(5) > button:nth-child(1)'));
     editDataButton.click();
     browser.sleep(1000);
+
     var inputField = element(by.css('body > div.geDialog > div > div:nth-child(1) > div > input[type="text"]'));
     inputField.sendKeys('abc');
     browser.sleep(1000);
+
     var addPropertyButton = element(by.css('body > div.geDialog > div > div:nth-child(1) > div > button'));
     addPropertyButton.click();
     browser.sleep(1000);
+
+    var showText = element(by.css('body > div.geDialog > div > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(1)'));
+    expect(showText.isDisplayed()).toBeTruthy();
+
     var rightBox1 = element(by.css('body > div.geDialog > div > div:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > textarea'));
     rightBox1.sendKeys(100);
     browser.sleep(1000);
+
     var enterPnameInput = element(by.css('body > div.geDialog > div > div:nth-child(1) > div > input[type="text"]'));
     enterPnameInput.sendKeys('xyz');
     addPropertyButton.click();
     browser.sleep(1000);
+
     var rightBox2 = element(by.css('body > div.geDialog > div > div:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2) > div > textarea'));
     rightBox2.sendKeys(200);
     browser.sleep(1000);
+
     var applyButton = element(by.className('geBtn gePrimaryBtn'));
     applyButton.click();
     browser.sleep(1000);
